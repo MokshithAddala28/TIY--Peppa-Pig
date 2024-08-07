265f0a6b06360d93637a38a4f88ec93956100167
Mokshith Addala <113805119+MokshithAddala28@users.noreply.github.com>
2024-08-07 19:22:03 -0400

    Finished exercise 1 and 2

---
+++ Exercise1.java
@@ -0,0 +1,15 @@
+public class Exercise1 {
+    public static void main(String[] args){
+    
+        int MUTABLE_INTEGERS = 2;
+        
+        final String  immutableVariable = "My Name is Mokshith";
+
+
+        System.out.println(MUTABLE_INTEGERS);
+        System.out.println(immutableVariable);
+    
+        };
+    };
+    
+
---
+++ Exercise2.java
@@ -0,0 +1,13 @@
+public class Exercise2 {
+    public static void main(String[] args){
+    
+        int x = 10;
+        int y = 20;
+        x++;
+        y--;
+        int z = x+y;
+        System.out.println(z);
+
+        };
+    };
+
