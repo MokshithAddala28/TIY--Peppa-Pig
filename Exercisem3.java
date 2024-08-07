e817409db889cfa8a0265bc9eb29776ff7a2ae07
Mokshith Addala <113805119+MokshithAddala28@users.noreply.github.com>
2024-08-07 19:46:07 -0400

    Finished Exercise 3

---
+++ Exercisem3.java
@@ -0,0 +1,17 @@
+
+
+public class Exercisem3 {
+    public static void main(String[] args){
+    int x =10;
+    if (x<100) {
+        x = x+50;
+    }
+    if (x>100){
+        x = x/20;
+    }
+    System.out.println(x);
+    
+        };
+    };
+    
+
